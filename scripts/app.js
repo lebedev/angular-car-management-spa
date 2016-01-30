79256b98252e7023ecfb6c3dd739767bcd846f12
zeird <l1aqus@gmail.com>
2016-01-30 16:01:20 +0600

    Add styleless list of 2 fixtured cars

---
+++ scripts/app.js
@@ -0,0 +1,32 @@
+(function() {
+'use strict';
+
+angular
+    .module('carManagement', ['ui.router'])
+    .run(function($rootScope) {
+        if (!localStorage.cars) {
+            localStorage.cars = JSON.stringify([
+                {
+                    vin: '5tdxk3dc8bs093588',
+                    make: 'Toyota',
+                    model: 'Sienna',
+                    year: 2011,
+                    trim: 'SE 8-Passenger',
+                    bodyType: 'Minivan',
+                    curbWeight: 4460
+                },
+                {
+                    vin: 'jf1znaa15d1702844',
+                    make: 'Scion',
+                    model: 'FR-S',
+                    year: 2013,
+                    trim: undefined,
+                    bodyType: 'Car',
+                    curbWeight: undefined
+                },
+            ]);
+        }
+        $rootScope.cars = JSON.parse(localStorage.cars);
+    });
+
+})();
