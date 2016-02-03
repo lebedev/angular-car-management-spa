35419ae416e34417c58ead5825add6c77145cb06
zeird <l1aqus@gmail.com>
2016-02-03 18:49:33 +0600

    Add lockscreen toggling to list ctrl

--- scripts/app.controllers.js
+++ scripts/app.controllers.js
@@ -5,12 +5,15 @@ angular
     .module('carManagement')
     .controller('listCtrl', function($rootScope, carService, $scope) {
         $rootScope.changeTitleTo('List');
+        $rootScope.setLockscreenStateTo(true);
         carService.getCarsList()
             .then(function(cars) {
                 $scope.cars = cars;
+                $rootScope.setLockscreenStateTo(false);
             })
             .catch(function(error) {
                 console.log(error);
+                $rootScope.setLockscreenStateTo(false);
             });
     });
 
